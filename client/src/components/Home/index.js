1ecd431f73da5c284fdd28be8910adc06ba00080
hak9292 <ak112997@gmail.com>
2022-09-22 09:11:13 -0400

    added animations and hovering effects for home page

---
+++ client/src/components/Home/index.js
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import AnimatedLetters from '../AnimatedLetters';
+import './index.scss';
+
+const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+
+    const nameArray = [' ', 'H', 'e', 'e', ' ', 'H', 'o', 'o', 'n', ' ', 'K', 'i', 'm']
+    const jobArray = ['w', 'e', 'b', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r', '.']
+
+    useEffect(() => {
+        setTimeout(() => {
+          setLetterClass('text-animate-hover')
+        }, 4000)
+      }, [])
+
+    return (
+        <>
+            <div className="container home-page">
+                <div className="text-zone">
+                    <h1>
+                        <span className={letterClass}>H</span>
+                        <span className={`${letterClass} _12`}>i,</span>
+                        <br />
+                        <span className={`${letterClass} _13`}>I</span>
+                        <span className={`${letterClass} _14`}>'m</span>
+                        <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={nameArray}
+                            idx={15}
+                        />
+                        <br />
+                        <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={jobArray}
+                            idx={28}
+                        />
+                    </h1>
+                    <h2>Full Stack Developer</h2>
+                    <Link to="/contact" className='flat-button'>
+                        Contact Me
+                        </Link>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home;
